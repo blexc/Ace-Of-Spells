959098a75439536647f526db6d119f1a77f45d54
Josh Knapp <joshdavidknapp@gmail.com>
2021-02-05 15:33:34 -0800

    camera bounds

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public int minX;
+    public int maxX;
+    public int minY;
+    public int maxY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,24 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x >= maxX)
+        {
+            transform.position = new Vector2(maxX, this.transform.position.y);
+        }
+
+        if (transform.position.x <= minX)
+        {
+            transform.position = new Vector2(minX, this.transform.position.y);
+        }
+
+        if (transform.position.x >= maxY)
+        {
+            transform.position = new Vector2(this.transform.position.x, maxY );
+        }
+
+        if (transform.position.x <= minY)
+        {
+            transform.position = new Vector2(this.transform.position.x, minY);
+        }
     }
 }
