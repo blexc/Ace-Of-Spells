1a17dc84df7858f07ee4de42de492c769fd1c7be
Josh Knapp <joshdavidknapp@gmail.com>
2021-02-10 16:11:14 -0800

    enemy follow

--- Assets/Scripts/Josh's Scripts/EnemyFollow.cs
+++ Assets/Scripts/Josh's Scripts/EnemyFollow.cs
@@ -4,35 +4,75 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
+    //enemy movement speed
     public float moveSpeed = 1;
+    //enemy detection radius
+    public float detectionRadius = 1;
+    //player reference
     GameObject player;
+    //enemy rigidbody reference
     private Rigidbody2D enemyRB;
-    private Vector2 movement;
+    //movement direcction
+    private Vector2 movementDirection;
+    //if enemy can follow player
+    public bool canFollow = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        //set detection radius
+        GetComponent<CircleCollider2D>().radius = detectionRadius;
+        //set player
         player = GameObject.FindGameObjectWithTag("Player");
+        //get enemy rigidbody
         enemyRB = GetComponent<Rigidbody2D>();
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
 
     private void FixedUpdate()
     {
+        //get player direction & rotate
         Vector3 direction = (player.transform.position - transform.position) * moveSpeed;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         enemyRB.rotation = angle;
         direction.Normalize();
-        movement = direction;
+        movementDirection = direction;
 
-        MoveCharacter(movement);
+        if(canFollow)
+        {
+            //move enemy
+            MoveCharacter(movementDirection);
+
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            canFollow = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canFollow = false;
+        }
     }
 
     public void MoveCharacter(Vector2 direction)
     {
+        //move enemy to player
         enemyRB.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
 }
