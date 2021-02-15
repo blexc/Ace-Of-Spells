b3a451b2c8313c60ef84fe75adfddbbc22db84e9
Alexander H. Lopez <alopez165@mail.csuchico.edu>
2021-02-15 11:44:08 -0800

    Update to the HP bar

--- Assets/Scripts/Lopez/GameplayUI.cs
+++ Assets/Scripts/Lopez/GameplayUI.cs
@@ -7,41 +7,27 @@ public class GameplayUI : MonoBehaviour
 {
     //Variable Initialization/Declaration
     public Image hpBarFull; //Full HP bar that will be adjusted when the player heals/takes damage
-    public float HPtemp = 200; //AHL - Temporary HP value until we have the actual player stats implemented
+    public int HPtemp = 100; //AHL - Temporary HP value until we have the actual player stats implemented
     public Text Hp; //Text that will hold and adjust the player health for the player to see during gameplay
     public Button Hurt; //AHL - Temporary button that will damage the health of the player to show the change.
+    
 
     //AHL - Temporary function to show how the damage works
     public void Damage()
     {
-        HPtemp -= 24f;
+        HPtemp -= 12;
     }
 
     /// <summary>
     /// Alexander Lopez (2/15/21): Function that takes in the player health and adjusts the health bar based on it. 
     /// </summary>
-    void HealthUpdate(float health)
+    void HealthUpdate(int health)
     {
-        if (health > 200)
-        {
-            Hp.text = "100";
-            hpBarFull.fillAmount = 200;
-            HPtemp = 200;
-        }
-        else if(health <= 200 && health > 0)
-        {
-            int stringHP = (int)health / 2;
-            Hp.text = "" + stringHP;
-            float HPadjustment = (health / 100.0f) * 180.0f / 360;
-            hpBarFull.fillAmount = HPadjustment;
-        }
-        else
-        {
-            Hp.text = "0";
-            hpBarFull.fillAmount = 0;
-            HPtemp = 0;
-        }
-            
+        HPtemp = Mathf.Clamp(health, 0, 100);
+        Hp.text = "" + health;
+        //AHL - Int to %
+        int HPBaradjustment = (health / 100) * 360;
+        hpBarFull.fillAmount = HPBaradjustment;
     }
 
     // Update is called once per frame
