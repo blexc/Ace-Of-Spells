18ca3635d48c240e8edc319a9993be57996d69b1
mgomez74 <62723321+mgomez74@users.noreply.github.com>
2021-03-09 09:17:31 -0800

    Animations

--- Assets/Scripts/Josh's Scripts/PlayerMovement.cs
+++ Assets/Scripts/Josh's Scripts/PlayerMovement.cs
@@ -6,11 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
    //player movement speed
     private float movementSpeed;
-
+    private Animator animator;
+    private SpriteRenderer sprite;
 
     private void Awake()
     {
         movementSpeed = GetComponent<PlayerStats>().moveSpeed;
+        animator = gameObject.GetComponent<Animator>();
+        sprite = gameObject.GetComponent<SpriteRenderer>();
     }
     private void FixedUpdate()
     {
@@ -25,13 +28,27 @@ public class PlayerMovement : MonoBehaviour
         {
             //Move Player Forward
             transform.position += transform.up * Time.deltaTime * movementSpeed;
+            animator.SetBool("back", true);
         }
+        else
+        {
+            animator.SetBool("back", false);
+        }
+
+
 
         //If Player Hits A
         if (Input.GetKey(KeyCode.A))
         {
             //Move Player Left
             transform.position += -transform.right * Time.deltaTime * movementSpeed;
+
+            animator.SetBool("side", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
         }
 
         //If Player Hits D
@@ -39,6 +56,14 @@ public class PlayerMovement : MonoBehaviour
         {
             //Move Player Right
             transform.position += transform.right * Time.deltaTime * movementSpeed;
+
+            animator.SetBool("side", true);
+        }
+
+        //set side animator bool to false if neither key are being pressed.
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("side", false);
         }
 
         //If Player Hits S
@@ -46,6 +71,24 @@ public class PlayerMovement : MonoBehaviour
         {
             //Move Player Back
             transform.position += -transform.up * Time.deltaTime * movementSpeed;
+            animator.SetBool("front", true);
+        }
+        else
+        {
+            animator.SetBool("front", false);
+        }
+
+        if (!Input.GetKey(KeyCode.S) && 
+            !Input.GetKey(KeyCode.D) &&
+            !Input.GetKey(KeyCode.A) &&
+            !Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("moving", false);
+        
+        }
+        else
+        {
+            animator.SetBool("moving", true);
         }
     }
 
