28113859e2148232144783ac39b53db389bce36b
Alexander H. Lopez <alopez165@mail.csuchico.edu>
2021-03-09 10:49:04 -0800

    Got the enemy damage popup working

--- Assets/Scripts/Enemy/Enemy UI/EnemyUI.cs
+++ Assets/Scripts/Enemy/Enemy UI/EnemyUI.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class EnemyUI : MonoBehaviour
 {
     //Variable Initialization/Declaration
     public Image HPBarFull; //Full HP bar that will be adjusted when the enemy heals/takes damage
+    public GameObject damageText; //Variable to hold the damage text
+
 
     [HideInInspector]
     public int HPMax; //Int for the maximum amount of hp that the enemy has
@@ -19,5 +22,17 @@ public class EnemyUI : MonoBehaviour
         int currHealth = Mathf.Clamp(health, 0, HPMax); //Sets health to be within a range of values
         float HPBaradjustment = (float)currHealth / (float)HPMax; //Gets a percantage of health remaining to adjust the HP UI
         HPBarFull.fillAmount = HPBaradjustment; //Adjusts the HP bar Image
+    }
+
+    /// <summary>
+    /// Function to spawn in the damage text prefab and adjust the number based on the damage the enemy took
+    /// </summary>
+    public void damagePopUP(int DamageAmount)
+    {
+        //Creates a temp GameObject of the current damage text to be adjusted
+        GameObject DamageTextInstance = Instantiate(damageText, this.gameObject.transform);
+
+        //Assigns the Damage Amount to the Text game object (which is the child of the DamageText GameObject)
+        DamageTextInstance.GetComponent<Transform>().GetComponentInChildren<TextMeshPro>().SetText(DamageAmount.ToString());   
     }
 }
