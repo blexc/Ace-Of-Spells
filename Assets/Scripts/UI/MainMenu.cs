5a6ee8882a9300f13c3a10ba5dcadcf77dadd48e
Alexander H. Lopez <alopez165@mail.csuchico.edu>
2021-03-10 16:50:03 -0800

    Fixed some issues with the Player Input

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Variable Declaration/Initialization
+    public string sceneName; //The name of the scene that the player will load in first
+
+    /// <summary>
+    /// Function that starts the game and sends the player to the scene specified - AHL (3/10/21)
+    /// </summary>
+    public void play()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
