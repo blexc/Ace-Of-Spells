c05cb4eae85a29d0cc3dc46f3e5899aa49e5cc92
alex <alex.tchap99@gmail.com>
2021-04-08 08:35:30 -0700

    Auto stash before merge of "AlexChapman" and "origin/main"

---
+++ Assets/Scripts/Spells/NovaSpell.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//spell that casts around the player and freezes enemies around the player.
+public class NovaSpell : Spell
+{
+}
