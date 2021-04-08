2a63ccbefe6d4c900d30c479a3b6f3ca101e3216
alex <alex.tchap99@gmail.com>
2021-04-08 09:10:34 -0700

    discard reward prefab and nova spell
    setup framework for 3 new frost spells

--- Assets/Scripts/Spells/NovaSpell.cs
+++ Assets/Scripts/Spells/NovaSpell.cs
@@ -4,4 +4,29 @@ using UnityEngine;
 //spell that casts around the player and freezes enemies around the player.
 public class NovaSpell : Spell
 {
+    /// <summary>
+    /// stick with the player
+    /// </summary>
+    public override void InitSpell()
+    {
+        var pa = FindObjectOfType<PlayerAttack>();
+        if (pa) transform.parent = pa.transform;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        //if hits an enemy
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            enemyHit = other.gameObject;
+            var eb = enemyHit.GetComponent<EnemyBase>();
+            eb.AddStatusEffect(StatusEffect.Freeze, effectlifeTime);
+            eb.FreezeCharacter(effectlifeTime);
+        }
+        else
+        {
+            // delete spell if hits anything other than an enemy
+            Destroy(gameObject);
+        }
+    }
 }
