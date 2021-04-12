5f0db30c0fbb877c403a765f181ef2a392523b09
Alexander H. Lopez <alopez165@mail.csuchico.edu>
2021-04-12 11:48:35 -0700

    The DeckList is on its tracks [WIP]

--- Assets/Scripts/UI/DeckList.cs
+++ Assets/Scripts/UI/DeckList.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class DeckList : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Card> allCards; //A list of all the cards currently in the Discard, Hand, and Draw piles
+    public Deck deck; //The deck script to keep track of the lists for the different card piles 
+
+    //Function is called when the UI becomes active - AHL (4/12/21)
+    private void OnEnable()
     {
-        
+        ShowSortedList();
     }
 
-    // Update is called once per frame
-    void Update()
+    //Function to remove all the cards 
+    private void OnDisable()
     {
-        
+        refreshList();
+    }
+
+    //Custom function to go through adding cards to the list and sorting it to be displayed
+    private void ShowSortedList()
+    {
+        foreach (Card card in deck.drawPile)
+        {
+            allCards.Add(card); //Adds the cards to the allCards list
+        }
+
+        foreach (Card card in deck.Hand)
+        {
+            allCards.Add(card); //Adds the cards to the allCards list
+        }
+
+        foreach (Card card in deck.discardPile)
+        {
+            allCards.Add(card); //Adds the cards to the allCards list
+        }
+
+        //Sorting the allCards list by on the card name
+        allCards = allCards.OrderBy(card => card.name).ToList();
+    }
+
+    //Function to remove all the cards within the allCards list so everything can be refreshed if the player collects or discards cards
+    private void refreshList()
+    {
+        allCards.Clear();
     }
 }
