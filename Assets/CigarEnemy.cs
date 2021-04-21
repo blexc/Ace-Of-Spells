f5c08de026b7fb7711cdf86160209305179b418b
Josh Knapp <joshdavidknapp@gmail.com>
2021-04-21 11:59:34 -0700

    cigar

---
+++ Assets/CigarEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CigarEnemy : MonoBehaviour
+{
+    public GameObject cigarBurn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CigarBurn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator CigarBurn()
+    {
+        Instantiate(cigarBurn, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(.5f);
+        StartCoroutine(CigarBurn());
+    }
+}
