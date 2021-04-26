1b6e08e7d7b1ecaff2b9ccd6aeb1ecc0d20738dc
alex <alex.tchap99@gmail.com>
2021-04-26 12:34:25 -0700

    framework of 3 new shadow spells

---
+++ Assets/Scripts/Spells/Shadow/DarknessSpell.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Spawns X cone projectiles, where X is the number of cards in discard.
+public class DarknessSpell : Spell
+{
+}
